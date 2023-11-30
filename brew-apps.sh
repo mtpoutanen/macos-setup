3a49611439dca8cac3a514cf6fbd02aff3009673
Mikko Poutanen <mtpoutanen@gmail.com>
2023-11-30 14:22:57 +0200

    Update brew-apps.sh

--- brew-apps.sh
+++ brew-apps.sh
@@ -1,1 +1,8 @@
+brew update
+brew upgrade
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install pyenv
+brew install nvm
+brew install openjdk
 brew install --cask signal
