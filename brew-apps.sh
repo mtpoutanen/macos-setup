3fe889247f57a9f5813504dc0dd5573374fb58bd
Mikko Poutanen <mtpoutanen@gmail.com>
2023-11-30 14:28:38 +0200

    Update brew-apps.sh

--- brew-apps.sh
+++ brew-apps.sh
@@ -1,5 +1,7 @@
 brew update
 brew upgrade
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install --cask iterm2
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 brew install pyenv
