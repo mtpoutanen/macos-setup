371ced9216f75803a0d2c50e475f7f413733db60
Mikko Poutanen <mtpoutanen@gmail.com>
2023-12-04 13:18:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ mkdir ~/.nvm
 source ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
 nvm install default
