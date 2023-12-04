a80c0d5394989fd5c70b8d6bd1f38f52c6d5d741
Mikko Poutanen <mtpoutanen@gmail.com>
2023-12-04 13:16:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ mv .zshrc ~
 mkdir ~/.nvm
 source ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 nvm install default
