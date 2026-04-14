5d029593fd9be98568c20e8bb8dba69071b6a233
Mikko Poutanen <ext-mikko.poutanen@posti.com>
2026-04-14 14:54:32 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-mv .zshrc ~
-mkdir ~/.nvm
-source ~/.zshrc
+cp .zshrc ~
+mkdir -p ~/.nvm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
@@ -8,3 +7,4 @@ cp ./robbyrussell.zsh-theme ~/.oh-my-zsh/theme/
 nvm install default
 pip install virtualenv
 sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk 
+source ~/.zshrc
