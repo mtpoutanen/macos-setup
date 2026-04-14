09c71ef412aaaf37889eec74b0c41fc8f89a69f2
Mikko Poutanen <ext-mikko.poutanen@posti.com>
2026-04-14 15:08:18 +0300

    Add zsh plugins for nvm and npm completion
    Added git clone commands for zsh-nvm and zsh-better-npm-completion plugins.

--- brew-apps.sh
+++ brew-apps.sh
@@ -4,6 +4,8 @@ eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 brew update
 brew upgrade
 brew install --cask iterm2
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 brew install pyenv
